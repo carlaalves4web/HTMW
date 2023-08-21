db9a434d2caccfd2738e380c7234c76e541c854e
Carla Alves <carlaalves08@gmail.com>
2023-08-21 12:33:22 -0300

    Update script.js
    Back-to-top

--- script.js
+++ script.js
@@ -15,15 +15,3 @@ $(document).ready(function(){
     });
 });
 
-const typed2 = document.querySelector('.typed2');
-typed2.classList.remove('typing2');
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.classList.add('typing2');
-            return;
-        }
-        typed2.classList.remove('typing2')
-    });
-});
-observer.observe(document.querySelector('.typed2'));
