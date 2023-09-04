53f47ebf2b0dba7b7a037686c1528f4081c6e8ce
Carla Alves <carlaalves08@gmail.com>
2023-09-04 16:00:40 -0300

    Update 09/04/2023

--- script.js
+++ script.js
@@ -1,64 +1,72 @@
+"use.strict"
 // Back-to-top animation
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-      $(`#back-to-top`).fadeIn();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+        $(`#back-to-top`).fadeIn();
+      } else {
+        $(`#back-to-top`).fadeOut();
+      }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+      $('body').animate(
+        {
+          scrollTop: 0,
+        },
+        400
+      );
+      return false;
+    });
+  });
+  
+  // Navbar
+  
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById('myNav').style.top = '0';
     } else {
-      $(`#back-to-top`).fadeOut();
+      document.getElementById('myNav').style.top = '-250px';
     }
-  });
-  // scroll body to 0px on click
-  $('#back-to-top').click(function () {
-    $('body').animate(
-      {
-        scrollTop: 0,
-      },
-      400
-    );
-    return false;
-  });
-});
-
-// Navbar
-
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('myNav').style.top = '0';
-  } else {
-    document.getElementById('myNav').style.top = '-250px';
   }
-}
 
+  // ScrollTrigger
 
-// Intersection Observer
+  function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector);
+    els = Array.from(els);
+    els.forEach(el => {
+      addObserver(el, options)
+    })
+  }
 
-const typing_animation = document.querySelectorAll('.typing')
-const observer = new IntersectionObserver((entries) => {
-  console.log(observer);
-  entries.forEach((entry) => {
-    if (entry.isIntersecting){
-      entry.target.classList.add('typing-animation')
-      
-    } else {
-      entry.target.classList.remove('typing-animation');
-    }
+  function addObserver(el, options){
+    let observer = new IntersectionObserver((entries, observer) =>{
+      entries.forEach(entry =>{
+        if(entry.isIntersecting){
+          if(options.cb){
+            options.cb(el)
+          }else{
+            entry.target.classList.add('active')
+          }
+          observer.observe(entry.target);
+        }
+      })
+    }, options)
+    observer.observe(el);
+  }
+
+  scrollTrigger('.mars-title', {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1 
+  });
+  scrollTrigger('.core-title', {
+    threshold: 1
+  });
+  scrollTrigger('.sample-title', {
+    threshold: 1
   })
-}, {
-  threshold: 0.5
-});
-
-for (let i=0; i<typing_animation.length; i++){
-  const elements = typing_animation[i];
-  console.log(elements)
-  observer.observe(elements);
-}
-
-const intersectionCallback = (entries, observer) => {
-  console.log(intersectionCallback);
-
-  // Your code for handling the intersection
-};
-
